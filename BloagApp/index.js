61725e42e10029bcd83d40dc45684b6fd9a0185a
pankajsalunke <pankajsalunke305@gmail.com>
2024-03-21 07:48:37 +0530

    blog app complete

---
+++ BloagApp/index.js
@@ -0,0 +1,26 @@
+const express  = require('express')
+const mongoose = require('mongoose')
+const articlerouter = require('./routes/articles')
+const dotenv = require("dotenv");
+const Article = require( './models/article');
+const methodoverride = require('method-override')
+const app = express()
+
+dotenv.config();
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.5cfikpj.mongodb.net/BlogAppDB`)
+
+app.use(express.urlencoded({extended:false}))
+app.use(methodoverride('_method'))
+
+app.set('view engine','ejs')
+
+app.get("/",async(req,res)=>{
+    const articles = await Article.find().sort( { createdAt:'desc' } )
+    res.render('articles/index',{articles:articles})
+})
+
+app.use('/articles',articlerouter)
+app.listen(3000)
